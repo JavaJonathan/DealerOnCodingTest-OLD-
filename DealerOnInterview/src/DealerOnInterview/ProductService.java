e6a61fdc20f3330bad85b4ac8df84f2d6acc6678
based <based@10.0.0.212>
2019-06-30 12:57:16 -0400

    Fixes Digit Error

--- DealerOnInterview/src/DealerOnInterview/ProductService.java
+++ DealerOnInterview/src/DealerOnInterview/ProductService.java
@@ -3,7 +3,7 @@ package DealerOnInterview;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-//product service will have a static API so it will not have to be instantiated and will make the code look cleaner
+//product service will have a static API so it will not have to be instantiated and make the code look cleaner
 public class ProductService 
 {
 	//array list to hold all products as added to order
